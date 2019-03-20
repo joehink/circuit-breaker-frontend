f2eb834347aa0a57ea9d5197bf98845658abd59f
Joe Hinkley <joehhinkley@gmail.com>
2019-03-19 22:40:25 -0700

    added new exercises

--- src/data/exercises.js
+++ src/data/exercises.js
@@ -1,27 +1,59 @@
 const exercises = [
   {
-    name: "Push Ups",
-    image: "images/exercises/pushups.png"
+    name: "Air Squats",
+    image: "images/exercises/airsquats.png"
   },
   {
-    name: "Jump Squats",
-    image: "images/exercises/jumpsquats.png"
+    name: "Alternate Heel Touchers",
+    image: "images/exercises/alternateheeltouchers.png"
   },
   {
-    name: "Plank",
-    image: "images/exercises/plank.png"
+    name: "Bear Crawls",
+    image: "images/exercises/bearcrawls.png"
+  },
+  {
+    name: "Bicycles",
+    image: "images/exercises/bicycles.png"
+  },
+  {
+    name: "Butt Kicks",
+    image: "images/exercises/buttkicks.png"
+  },
+  {
+    name: "Calf Raises",
+    image: "images/exercises/calfraises.png"
+  },
+  {
+    name: "Crunches",
+    image: "images/exercises/crunches.png"
   },
   {
     name: "Jumping Jacks",
     image: "images/exercises/jumpingjacks.png"
   },
   {
-    name: "Lunges",
-    image: "images/exercises/lunges.png"
+    name: "Jump Squats",
+    image: "images/exercises/jumpsquats.png"
   },
   {
     name: "Leg Raises",
     image: "images/exercises/legraises.png"
+  },
+  {
+    name: "Lunges",
+    image: "images/exercises/lunges.png"
+  },
+  {
+    name: "Plank",
+    image: "images/exercises/plank.png"
+  },
+  {
+    name: "Push Ups",
+    image: "images/exercises/pushups.png"
+  },
+  {
+    name: "Sit Ups",
+    image: "images/exercises/situps.png"
   }
 ]
 
